bdc0bcf6b32d527f04897a0319f55515344b0299
Nico Larena <nic.lar.universidad@gmail.com>
2022-08-21 17:40:48 -0300

    TimeServiceTest added

---
+++ src/test/java/com/cl/evaluation/register/services/TimeServiceTest.java
@@ -0,0 +1,36 @@
+package com.cl.evaluation.register.services;
+
+import com.cl.evaluation.register.ApplicationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class TimeServiceTest extends ApplicationTest {
+
+    private final TimeService timeService;
+
+    @Autowired
+    public TimeServiceTest(TimeService timeService) {
+        this.timeService = timeService;
+    }
+
+    @Test
+    public void getCurrentTimeTest() {
+        var date = timeService.getCurrentTime();
+        assertThat(date, equalTo(new Date()));
+    }
+
+    @Test
+    public void toTimestampTest() {
+        var date = new Date(System.currentTimeMillis());
+        var dateConverted = timeService.toTimestamp(date);
+        var timestamp = new Timestamp(date.getTime());
+
+        assertThat(dateConverted, equalTo(timestamp));
+    }
+}
